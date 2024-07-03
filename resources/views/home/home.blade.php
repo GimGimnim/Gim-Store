ab21aa750db9746105f63633a2a551cb94f69774
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2024-07-03 10:09:40 +0100

    Update home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -8,14 +8,11 @@
 </head>
 
 <body style="background-color: rgba(255,229,180,0.2);">
-  <div class="hero_area">
     
   @include('home.header')
 
   @include('home.slider')
-
-  </div>
-
+    
   @include('home.body')
 
   @include('home.services')
